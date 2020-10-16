faa3fc3121aa8e938d5a856043f89078af8fcbbd
Joe Webster <joseph.scott.webster@gmail.com>
2020-10-16 16:08:51 -0400

    Show random image cover on load

--- src/main.js
+++ src/main.js
@@ -1,17 +1,23 @@
 // Create variables targetting the relevant DOM elements here 👇
+let coverImage = document.querySelector('.js-cover-image');
+let title = document.querySelector('.js-cover-title');
+let tagline1 = document.querySelector('.js-tagline-1');
+let tagline2 = document.querySelector('.js-tagline-2');
 
 
 // We've provided a few variables below
-var savedCovers = [
-  new Cover("http://3.bp.blogspot.com/-iE4p9grvfpQ/VSfZT0vH2UI/AAAAAAAANq8/wwQZssi-V5g/s1600/Do%2BNot%2BForsake%2BMe%2B-%2BImage.jpg", "Sunsets and Sorrows", "sunsets", "sorrows")
-];
-var currentCover;
+// let savedCovers = [
+//   new Cover("http://3.bp.blogspot.com/-iE4p9grvfpQ/VSfZT0vH2UI/AAAAAAAANq8/wwQZssi-V5g/s1600/Do%2BNot%2BForsake%2BMe%2B-%2BImage.jpg", "Sunsets and Sorrows", "sunsets", "sorrows")
+// ];
+// let currentCover;
 
 // Add your event listeners here 👇
-
+document.addEventListener('onload', loadMainCoverData());
 
 // Create your event handlers and other functions here 👇
-
+function loadMainCoverData() {
+  coverImage.src = covers[getRandomIndex(covers)];
+}
 
 // We've provided one function to get you started
 function getRandomIndex(array) {
